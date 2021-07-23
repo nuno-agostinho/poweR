04b3ff2f741b0e13125f926ae9435bb7572d7234
Nuno Agostinho <nunodanielagostinho@gmail.com>
2021-07-23 10:56:22 +0100

    Create square and cube functions

---
+++ R/cube.R
@@ -0,0 +1,14 @@
+#' Calculate cube of a number
+#'
+#' In other words, return the power of three for a given number.
+#'
+#' @param x Numeric
+#'
+#' @return Number
+#' @export
+#'
+#' @examples
+#' cube(5)
+cube <- function(x) {
+  x ** 3
+}
---
+++ R/square.R
@@ -0,0 +1,14 @@
+#' Calculate square of a number
+#'
+#' In other words, return the power of two for a given number.
+#'
+#' @param x Numeric
+#'
+#' @return Number
+#' @export
+#'
+#' @examples
+#' square(5)
+square <- function(x) {
+  x ** 2
+}
